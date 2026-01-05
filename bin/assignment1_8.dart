ea88795bba7d49cea05218e4ac019d5014655e76
ABHabibBD <109722098+ABHabibBD@users.noreply.github.com>
2026-01-05 21:29:30 +0600

    Assignment: Working with Sets in Dart

--- bin/assignment1_8.dart
+++
@@ -1,5 +0,0 @@
-import 'package:assignment1_8/assignment1.dart' as assignment1_8;
-
-void main(List<String> arguments) {
-  print('Hello world: ${assignment1_8.calculate()}!');
-}
--- lib/assignment8.dart
+++ lib/assignment8.dart
@@ -5,8 +5,8 @@ void main() {
   print("Assignment 8 — Stop a Loop Using Break");
 
   for (int num = 1; num <= 10; num++) {
-    if (num == 7) {
-      print("Loop last num = $num");
+    if (num == 8) {
+      print("last num = $num");
       break; // লুপ এখানেই থেমে যাবে
     }
     print(num);
